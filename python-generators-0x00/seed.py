211bfab0ba0c17942a405ab83e9c8cdf12afca7a
Kenny Dasilva <kennydasilva542@gmail.com>
2025-10-31 09:11:26 +0200

    addiction of method of creating the database

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -18,3 +18,18 @@ def connect_db():
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
+    
+
+#create database ALX_prodev if not exists
+
+def create_database(connection):
+
+    try:
+        curor=connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev;")
+        cursor.close()
+
+    except mysql.connector.Error as err:
+        print(f"Error creting database: {err}")
+
+
