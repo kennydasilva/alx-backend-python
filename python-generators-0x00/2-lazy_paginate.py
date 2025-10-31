2f3640d1af2414e6e6f6817667333903bddbce96
Kenny Dasilva <kennydasilva542@gmail.com>
2025-10-31 14:48:11 +0200

    upload of paginate method

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -6,4 +6,16 @@ def paginate_users(page_size, offset):
 
     """Fecth  one page of users from the database"""
 
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT 8FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+
+def lazy_pagination(page_size):
+
+    """ Generation that lazily loads paginated data from the database"""
     
