b7c816ee67c432e517b29d183f2d918cf4855674
Kenny Dasilva <kennydasilva542@gmail.com>
2025-10-31 15:15:25 +0200

    update of the stream age

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 seed = __import__('seed')
+
+
+def stream_user_ages():
+    try:
+        """Generator that yields user ages one by one from the database"""
+        connection = seed.connect_to_prodev()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT age FROM user_data")
+
+        for(age,) in cursor:
+            yield age
+        cursor.close()
+        connection.close()
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
