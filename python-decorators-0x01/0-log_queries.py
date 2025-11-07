9ca535aba7e1dfa2e0c32f503370ae9970d704ef
Kenny Dasilva <kennydasilva542@gmail.com>
2025-11-07 17:20:41 +0200

    fix bug

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -28,8 +28,8 @@ def fetch_all_users(query):
     conn.close()
     return reults
 
-    users = fetch_all_users(query="SELECT*FROM users")
-    print(users)
+users = fetch_all_users(query="SELECT*FROM users")
+print(users)
 
 
 
