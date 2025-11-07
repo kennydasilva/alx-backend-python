214397c7c438bbb183f93f25974c116cf9259445
Kenny Dasilva <kennydasilva542@gmail.com>
2025-11-07 13:28:47 +0200

    decorator log query

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -6,6 +6,14 @@ import functools
 #Decorator to log sql queries
 def log_queries(func):
     @functools.wraps(func)
-    def wrapper(*args, 88kwargs):
+    def wrapper(*args, **kwargs):
 
         #check if 'query' was passed either possitionalty or as a keyword argument
+        query =kwargs.get('query',None)
+
+        if query is None and len(args) > 0:
+            query = args[0]
+
+            print(f"Executing query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
