8e8eba1f7c51c6d48817d08af625b195f26e07c0
Kenny Dasilva <kennydasilva542@gmail.com>
2025-11-07 17:49:31 +0200

    bug of exercise1 fixed

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -3,32 +3,36 @@ import sqlite3
 import functools
 
 
-
 def log_queries(func):
+    """Decorator to log SQL queries before executing them"""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        # Verifica se 'query' foi passada como argumento nomeado
+        query = kwargs.get('query', None)
 
-        #check if 'query' was passed either possitionalty or as a keyword argument
-        query =kwargs.get('query',None)
-
+        # Caso não tenha sido, tenta obter o primeiro argumento posicional
         if query is None and len(args) > 0:
             query = args[0]
 
+        # Faz o log da query antes da execução
+        if query is not None:
             print(f"Executing query: {query}")
+
+        # Executa a função original
         return func(*args, **kwargs)
     return wrapper
 
 
 @log_queries
 def fetch_all_users(query):
-    conn=sqlite3.connect('users.db')
-    cursor=conn.cursor()
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
     cursor.execute(query)
-    reults=cursor.fetchall()
+    results = cursor.fetchall()
     conn.close()
-    return reults
-    users = fetch_all_users(query="SELECT*FROM users")
-    print(users)
+    return results
 
 
 
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
