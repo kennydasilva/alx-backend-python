0fdda35b01e697a9985245ef2e0b54af34b918ba
Kenny Dasilva <kennydasilva542@gmail.com>
2025-11-14 22:28:42 +0200

    bug solved

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,8 +49,8 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
 
         with patch(
-            'utils.requests.get',
-            return_value=mock_response
+                'utils.requests.get',
+                return_value=mock_response
         ) as mock_get:
 
             result = get_json(test_url)
@@ -76,9 +76,9 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(
-            TestClass,
-            "a_method",
-            return_value=42
+                TestClass,
+                "a_method",
+                return_value=42
         ) as mock_method:
 
             test_instance = TestClass()
