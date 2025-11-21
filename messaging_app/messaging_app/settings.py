fa187370f6b8eef67a5c252b3ad730dbdbb4b6b1
Kenny Dasilva <kennydasilva542@gmail.com>
2025-11-21 09:22:06 +0200

    fix of bug

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,12 +41,12 @@ INSTALLED_APPS = [
     'chats',  
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',  
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',  
-        'rest_framework.authentication.BasicAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ],
 }
 
