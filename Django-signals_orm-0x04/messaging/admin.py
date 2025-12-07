35ec6c4d4d3d56e8023e244fc3d4c26a29b1796e
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-07 13:29:14 +0200

    addiction of messagehistory

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 from .models import Message, Notification
 
+class MessageHistoryInline(admin.TabularInline):
+    model = MessageHistory
+    extra = 0
+    readonly_fields = ('old_content', 'edited_at', 'edited_by')
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'sender', 'receiver', 'content', 'timestamp', 'read')
     list_filter = ('read', 'timestamp')
     search_fields = ('sender__username', 'receiver__username', 'content')
+    inlines = [MessageHistoryInline]
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'message', 'text', 'created_at', 'is_read')
     list_filter = ('is_read', 'created_at')
     search_fields = ('user__username', 'text')
+
+
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'edited_at', 'edited_by')
+    search_fields = ('message__content', 'old_content', 'edited_by__username')
+    readonly_fields = ('old_content', 'edited_at', 'edited_by')
